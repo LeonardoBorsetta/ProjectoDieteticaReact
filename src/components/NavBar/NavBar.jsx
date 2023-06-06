8e4593a11ec2a41e61f4a6e8cce2bb65074eba02
Leonardo Borsetta <leonardoborsetta@gmail.com>
2023-06-06 14:30:18 -0500

    con todos los icons

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,6 +3,11 @@ import "./navBar.css"
 import handleClick from '../../hooks/handleNavBarClick'
 import toogleNavBar from '../../hooks/toogleNavBar'
 import homeIcon from '../../assets/navBarimg/homeIcon.png'
+import historyIcon from '../../assets/navBarimg/historyIcon.png'
+import productIcon from '../../assets/navBarimg/productsIcon.png'
+import servicesIcon from '../../assets/navBarimg/serviceIcon.png'
+import contactIcon from '../../assets/navBarimg/contactoIcon.png'
+import partnersIcon from '../../assets/navBarimg/partnersIcon.png'
 
 function NavBar() {
 
@@ -18,11 +23,11 @@ function NavBar() {
       </div>
       <div className="icons-container">
         <img onClick={handleClick} src={homeIcon} alt="To Home" className='navBar-icon' /><p className='navBar-icon-desc' onClick={handleClick} id="To Home">To Home</p>
-        <img onClick={handleClick} src="src\assets\navBarimg\historyIcon.png" alt="To History" id="To History"  className='navBar-icon' /><p className='navBar-icon-desc' onClick={handleClick} id="To History">To History</p>
-        <img onClick={handleClick} src="src\assets\navBarimg\productsIcon.png" alt="To Products" id="To Products" className='navBar-icon' /><p className='navBar-icon-desc' onClick={handleClick} id="To Products">To Products</p>
-        <img onClick={handleClick} src="src\assets\navBarimg\serviceIcon.png" alt="To Services"  className='navBar-icon' /><p className='navBar-icon-desc' onClick={handleClick} id='To Services'>To Services</p>
-        <img onClick={handleClick} src="src\assets\navBarimg\contactoIcon.jpg" alt="To Contact"  className='navBar-icon' /><p className='navBar-icon-desc' onClick={handleClick} id="To Contact">To Contact</p>
-        <img onClick={handleClick} src="src\assets\navBarimg\partnersIcon.png" alt="To Partners"  className='navBar-icon' /><p className='navBar-icon-desc' onClick={handleClick} id="To Partners">To Partners</p>
+        <img onClick={handleClick} src={historyIcon} alt="To History" id="To History"  className='navBar-icon' /><p className='navBar-icon-desc' onClick={handleClick} id="To History">To History</p>
+        <img onClick={handleClick} src={productIcon} alt="To Products" id="To Products" className='navBar-icon' /><p className='navBar-icon-desc' onClick={handleClick} id="To Products">To Products</p>
+        <img onClick={handleClick} src={servicesIcon} alt="To Services"  className='navBar-icon' /><p className='navBar-icon-desc' onClick={handleClick} id='To Services'>To Services</p>
+        <img onClick={handleClick} src={contactIcon} alt="To Contact"  className='navBar-icon' /><p className='navBar-icon-desc' onClick={handleClick} id="To Contact">To Contact</p>
+        <img onClick={handleClick} src={partnersIcon} alt="To Partners"  className='navBar-icon' /><p className='navBar-icon-desc' onClick={handleClick} id="To Partners">To Partners</p>
       </div>
       </div>
   </>
